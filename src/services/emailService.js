45d0e812dd303284620b012c29ca49f4fd1fb6cf
obaraelijah <elijahobara357@gmail.com>
2024-01-09 19:53:40 +0300

    chore: add services

---
+++ src/services/emailService.js
@@ -0,0 +1,59 @@
+const config = require('../config/index');
+const logger = require('../config/_winstonLogger');
+const nodemailer = require('nodemailer');
+
+const createTransporter = () => {
+  return nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: config.EMAIL,
+      pass: config.PASSWORD,
+    },
+  });
+};
+
+/**
+ * @desc  Function to send the verification email
+ * @param {string} email - Recipient's email address
+ * @param {string} verificationLink - Verification link
+ */
+const sendVerificationEmail = async (email, verificationLink) => {
+  try {
+    const transporter = createTransporter();
+    const mailOptions = {
+      from: 'support@storylink.com',
+      to: email,
+      subject: 'Account Verification',
+      text: `Please click the following link to verify your account: ${verificationLink}`,
+    };
+    const info = await transporter.sendMail(mailOptions);
+    logger.info('Verification email sent:', { response: info.response });
+  } catch (error) {
+    logger.error('Error sending verification email:', error);
+  }
+};
+
+/**
+ * @desc  Function to send the password reset email
+ * @param {string} email - Recipient's email address
+ * @param {string} resetPasswordLink - Password reset link
+ */
+const sendResetPasswordLink = async (email, resetPasswordLink) => {
+  try {
+    const transporter = createTransporter();
+    const mailOptions = {
+      from: 'support@storylink.com',
+      to: email,
+      subject: 'Password Reset',
+      text: `Please click the following link to reset your password: ${resetPasswordLink}`,
+    };
+    const info = await transporter.sendMail(mailOptions);
+    logger.info('Password reset link sent:', { response: info.response });
+  } catch (error) {
+    logger.error('Error sending password reset email:', error);
+  }
+};
+
+module.exports = { sendVerificationEmail, sendResetPasswordLink };
