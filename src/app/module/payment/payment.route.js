f1d38e7b77735ce09eab614ed67100ca5b3b9674
obaraelijah <elijahobara357@gmail.com>
2024-01-10 20:05:43 +0300

    feat: payment module

---
+++ src/app/module/payment/payment.route.js
@@ -0,0 +1,12 @@
+// Basic Lib consts
+const express = require('express');
+const router = express.Router();
+
+const { createOrder } = require('./payment.controller');
+
+const { authMiddleware } = require('../../middleware/authMiddleware');
+
+// Routing Implement
+router.post('/charge/create-order', authMiddleware, createOrder);
+
+module.exports = router;
