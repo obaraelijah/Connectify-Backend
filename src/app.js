f6f0b86690544c92d44855d8cf21754084a3dd57
obaraelijah <elijahobara357@gmail.com>
2024-01-10 20:09:46 +0300

    chore: configure main app routes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { errorHandler, notFound } = require('./app/middleware/errorMiddleware');
 
+const applicationRoutes = require('./app/routes/index');
 
 const app = express();
 
@@ -17,7 +19,26 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors('*'));
 
+// // Define an array of allowed origins
+// const allowedOrigins = [
+//   `http://localhost:5173`,
+//   `https://leafline-admin.vercel.app/`,
+//   `https://leafline.vercel.app/`,
+//   // Add more domains as needed
+// ];
 
+// // Configure CORS with the allowed origins
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Check if the origin is in the list of allowed origins or if it's undefined (for same-origin requests)
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true); // Allow the request
+//     } else {
+//       callback(new Error('Not allowed by CORS')); // Deny the request
+//     }
+//   },
+//   credentials: true,
+// }));
 
 app.use(
   morgan('common', {
@@ -31,12 +52,13 @@ app.use(
     extended: false,
   }),
 );
-
+app.use('/api/v1/', applicationRoutes);
 app.use('/', (req, res) => {
   res
     .status(200)
     .json({ statusCode: 200, success: true, message: 'Health OK' });
 });
-
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
